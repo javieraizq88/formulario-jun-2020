7930b12858083142f5114e399736859f75a3edfa
Javiera Izquierdo M <javieraizquierdo7@gmail.com>
2020-06-24 17:14:34 -0400

    home CSS y botones actualizados

--- src/home.js
+++ src/home.js
@@ -6,10 +6,10 @@ import Header from './header';
 const Home = props => {
 
     return (
-        <div >
-<Header />
+        <div id="container">
+            <Header />
             <h2 id="subtitulo-inicio">Para inicar la encuesta, haz click en el siguiente botón</h2>
-            <Link to="/pregunta1"><button id="boton-inicio"  type="button" className="btn btn-success">Comenzar</button></Link>
+            <Link to="/pregunta1"><button id="boton-inicio" type="button" className="btn btn-success">Comenzar</button></Link>
 
         </div>
     )
