65d698c92c85d3fb68f13be5274ddb9ae81cf742
Javiera Izquierdo M <javieraizquierdo7@gmail.com>
2020-06-24 16:34:22 -0400

    pregunta 2 con CSS y botones listo, falta renderizado

--- src/1.js
+++ src/1.js
@@ -4,36 +4,35 @@ import { Link, useHistory } from 'react-router-dom';
 import Header from './header';
 
 const Formulario1 = props => {
-
     const history = useHistory();
 
     return (
-<div id="container">
-    <Header />
+        <div id="container">
+            <Header />
 
-        <div >
-                      <h3 id="subtitulo">Pregunta 1 de 4</h3>
+            <div >
+                <h3 id="subtitulo">Pregunta 1 de 4</h3>
 
-            <div className="container">
-                <div className="row">
-                    <div className="col">
-                        <form id="loginForm">
-                            <div id="error" className="alert alert-danger" role="alert">
-                            </div>
-                            <div className="col-md-5">
-                                <label id="pregunta1" for="nombre">¿Cuál es tu nombre?</label>
-                                <input type="text" className="form-control" id="nombre" placeholder="Nombre y Apellido" value="" />
-                                <br />
-                            </div>
-                        </form>
-                                <div className="" id="botones">
-<button id="boton-volver" type="button" onClick={() => history.goBack()} className="btn btn-success float-left">Volver</button>
+                <div className="container">
+                    <div className="row">
+                        <div className="col">
+                            <form id="loginForm">
+                                <div id="error" className="alert alert-danger" role="alert">
+                                </div>
+                                <div className="col-md-5">
+                                    <label id="pregunta1" for="nombre">¿Cuál es tu nombre?</label>
+                                    <input type="text" className="form-control" id="nombre" placeholder="Nombre y Apellido" value="" />
+                                    <br />
+                                </div>
+                            </form>
+                            <div className="" id="botones">
+                                <button id="boton-volver" type="button" onClick={() => history.goBack()} className="btn btn-success float-left">Volver</button>
                                 <Link to="/pregunta2"><button id="boton-siguiente" type="button" className="btn btn-success">Siguiente</button></Link>
                             </div>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
         </div>
     )
 }
--- src/2.js
+++ src/2.js
@@ -1,10 +1,14 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
+import Header from "./header";
 
 const Preg2 = props => {
+    const history = useHistory();
+
     return (
         <div id="container">
-            <h1 id="titulo" className="" >Encuesta Cadem</h1>
+            <Header />
+
             <h3 id="subtitulo">Pregunta 2 de 4</h3>
 
             <div className="container">
@@ -14,12 +18,15 @@ const Preg2 = props => {
                             <div id="error" className="alert alert-danger" role="alert">
                             </div>
                             <div class="form-group col-md-3">
-                                <label id="pregunta" for="nombre">¿Qué edad tienes nombre P1?</label>
-                                <input type="text" className="form-control" id="nombre" placeholder="" value="" />
+                                <label id="pregunta2" for="nombre">¿Qué edad tienes nombre P1?</label>
+                                <input type="text" className="form-control" id="edad" placeholder="" value="" />
                                 <br />
-                                <Link to="/pregunta3"><button id="boton-siguiente" type="button" className="btn btn-success">Siguiente</button></Link>
                             </div>
                         </form>
+                        <div className="" id="botones">
+                            <button id="boton-volver" type="button" onClick={() => history.goBack()} className="btn btn-success float-left">Volver</button>
+                            <Link to="/pregunta3"><button id="boton-siguiente" type="button" className="btn btn-success">Siguiente</button></Link>
+                        </div>
                     </div>
                 </div>
             </div>
