6b548e9cbfdd883007dbc7e71bc82af9d3ab2318
Javiera Izquierdo M <javieraizquierdo7@gmail.com>
2020-06-24 16:29:28 -0400

    boton volver y siguiente con CSS

--- src/1.js
+++ src/1.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import './App.css';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
+import Header from './header';
 
 const Formulario1 = props => {
 
+    const history = useHistory();
+
     return (
-        <div id="container">
-            <h1 id="titulo" className="" >Encuesta Cadem</h1>
-            <h3 id="subtitulo">Pregunta 1 de 4</h3>
+<div id="container">
+    <Header />
+
+        <div >
+                      <h3 id="subtitulo">Pregunta 1 de 4</h3>
 
             <div className="container">
                 <div className="row">
@@ -16,15 +21,19 @@ const Formulario1 = props => {
                             <div id="error" className="alert alert-danger" role="alert">
                             </div>
                             <div className="col-md-5">
-                                <label id="pregunta" for="nombre">¿Cuál es tu nombre?</label>
+                                <label id="pregunta1" for="nombre">¿Cuál es tu nombre?</label>
                                 <input type="text" className="form-control" id="nombre" placeholder="Nombre y Apellido" value="" />
                                 <br />
-                                <Link to="/pregunta3"><button id="boton-siguiente" type="button" className="btn btn-success">Siguiente</button></Link>
                             </div>
                         </form>
+                                <div className="" id="botones">
+<button id="boton-volver" type="button" onClick={() => history.goBack()} className="btn btn-success float-left">Volver</button>
+                                <Link to="/pregunta2"><button id="boton-siguiente" type="button" className="btn btn-success">Siguiente</button></Link>
+                            </div>
                     </div>
                 </div>
             </div>
+        </div>
         </div>
     )
 }
